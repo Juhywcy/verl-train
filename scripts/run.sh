88db27ae46f51b707f47c5403488e0af030b478b
朱厚润 <9206072+zhu-hourun@user.noreply.gitee.com>
2025-12-27 15:41:40 +0800

    y

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-bash verl/recipe/sfmx/dapo_grad_damping.sh 
-bash verl/recipe/sfmx/dapo.sh
+# bash verl/recipe/sfmx/dapo_grad_damping.sh 
+# bash verl/recipe/sfmx/dapo.sh
+bash verl/recipe/sfmx/dapo_grad_damping_eos.sh

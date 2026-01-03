1198888c7bfddffc26c95001ae1c2ae86588d9b8
朱厚润 <9206072+zhu-hourun@user.noreply.gitee.com>
2026-01-04 01:01:27 +0800

    y

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,6 +3,15 @@
 # bash verl/recipe/sfmx/dapo_grad_damping.sh 
 # bash verl/recipe/sfmx/dapo.sh
 
+# while true; do
+#     bash scripts/wait.sh
+# done
+
 while true; do
+    val=$(cat ckpts/DAPO/DAPO-deepseek-Qwen1.5B-100step-grad-damping-eos/latest_checkpointed_iteration.txt)
+    if [ "$val" -eq 300 ]; then
+        echo "Value is 300. Stopping."
+        break
+    fi
     bash scripts/wait.sh
 done

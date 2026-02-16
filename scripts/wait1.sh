966f8770b65caf6a9345a9d76d2495a4b34e36ba
Juhywcy <1464142626@qq.com>
2026-02-16 11:59:21 +0800

    y

--- scripts/wait1.sh
+++ scripts/wait1.sh
@@ -27,7 +27,7 @@ while true; do
         
 
        
-        bash verl/recipe/ig/grpo.sh        
+        bash verl/recipe/l2s/qwen3_4B.sh       
         break
     else
         echo "GPU $GPU_ID 当前显存 ${USED_MEM} MiB，大于等于阈值 ${THRESHOLD} MiB，等待中..."

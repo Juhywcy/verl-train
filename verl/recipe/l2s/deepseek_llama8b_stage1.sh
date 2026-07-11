4d696466cca460c861ae8d9b598a99f502871380
zhuhourun <zhuhourun@bytedance.com>
2026-07-12 03:40:28 +0800

    y

--- verl/recipe/l2s/deepseek_llama8b_stage1.sh
+++ verl/recipe/l2s/deepseek_llama8b_stage1.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-export CUDA_VISIBLE_DEVICES="0,1,2,3" 
+# export CUDA_VISIBLE_DEVICES="0,1,2,3" 
 # export CUDA_VISIBLE_DEVICES="4,5,6,7" 
 # export CUDA_VISIBLE_DEVICES="4,5"
 # export RAY_DEBUG_POST_MORTEM=1
